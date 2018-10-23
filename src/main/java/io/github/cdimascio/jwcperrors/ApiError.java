287b81e0ae85c3fb3e05787361056461fed2a966
Carmine DiMascio <dimascio@us.ibm.com>
2018-10-22 20:03:32 -0400

    add documentation and addition http errors

--- src/main/java/io/github/cdimascio/jwcperrors/ApiError.java
+++ src/main/java/io/github/cdimascio/jwcperrors/ApiError.java
@@ -27,20 +27,38 @@ public class ApiError extends RuntimeException {
         super();
     }
 
+    /**
+     * A general api error
+     * @param trace A UUID that uniquely identifies this api error instance
+     * @param errors A list of error details
+     * @param status The http error status accompanying this error
+     */
     public ApiError(String trace, List<ApiErrorDetails> errors, HttpStatus status) {
         this.trace = trace;
         this.errors = errors;
         this.status = status;
     }
 
+    /**
+     * Returns the uuid for this ApiError
+     * @return The ApiError
+     */
     public String getTrace() {
         return trace;
     }
 
+    /**
+     * Returns the list of errors for this ApiError
+     * @return The ApiError
+     */
     public List<ApiErrorDetails> getErrors() {
         return this.errors;
     }
 
+    /**
+     * Returns the http status for this ApiError
+     * @return The ApiError
+     */
     public HttpStatus getStatus() {
         return status;
     }
@@ -54,75 +72,261 @@ public class ApiError extends RuntimeException {
                 '}';
     }
 
+    /**
+     * Create a bad request api error using the specified throwable
+     * @param t The exceptioo or throwable
+     * @return The ApiError
+     */
     public static ApiError badRequest(Throwable t) {
         return error(HttpStatus.BAD_REQUEST, t);
     }
+
+    /**
+     * Creates a bad request api error with the specified message
+     * @param message The message
+     * @return The ApiError
+     */
     public static ApiError badRequest(String message) {
         return error(HttpStatus.BAD_REQUEST, message);
     }
+
+    /**
+     * Creates a bad request api error
+     * @return The ApiError
+     */
     public static ApiError badRequest() {
         return error(HttpStatus.BAD_REQUEST, "bad request");
     }
+
+    /**
+     * Creates a conflict api error with the specified exception or throwable
+     * @param t The exception or throwable
+     * @return The ApiError
+     */
     public static ApiError conflict(Throwable t) {
         return error(HttpStatus.CONFLICT, t);
     }
+
+    /**
+     * Creates a conflict api error with the specified message
+     * @param message The message
+     * @return The ApiError
+     */
     public static ApiError conflict(String message) {
         return error(HttpStatus.CONFLICT, message);
     }
+
+    /**
+     * Creates a conflict error
+     * @return The ApiError
+     */
     public static ApiError conflict() {
         return error(HttpStatus.CONFLICT, "conflict");
     }
+
+    /**
+     * Create a forbidden error using the specified exception or throwable
+     * @param t The exception or throwable
+     * @return The ApiError
+     */
     public static ApiError forbidden(Throwable t) {
         return error(HttpStatus.FORBIDDEN, t);
     }
+
+    /**
+     * Creates a forbidden error with the specified message
+     * @param message The message
+     * @return The ApiError
+     */
     public static ApiError forbidden(String message) {
         return error(HttpStatus.FORBIDDEN, message);
     }
+
+    /**
+     * Creates a forbidden error
+     * @return The ApiError
+     */
     public static ApiError forbidden() {
         return error(HttpStatus.FORBIDDEN, "forbidden");
     }
+
+    /**
+     * Creates an internal server error with the specified exception or throwable
+     * @param t The exception or throwable
+     * @return The ApiError
+     */
     public static ApiError internalServerError(Throwable t) {
         return error(HttpStatus.INTERNAL_SERVER_ERROR, t);
     }
+
+    /**
+     * Creates an internal server error with the specified message
+     * @param message
+     * @return The ApiError
+     */
     public static ApiError internalServerError(String message) {
         return error(HttpStatus.INTERNAL_SERVER_ERROR, message);
     }
+
+    /**
+     * Creates an internal server error
+     * @return The ApiError
+     */
     public static ApiError internalServerError() {
         return error(HttpStatus.INTERNAL_SERVER_ERROR, "internal server error");
     }
+
+    /**
+     * Creates a not acceptable error with the specified exception or throwable
+     * @param t The exception or throwable
+     * @return The ApiError
+     */
+    public static ApiError notAcceptable(Throwable t) {
+        return error(HttpStatus.NOT_ACCEPTABLE, t);
+    }
+    /**
+     * Creates a not acceptable error with the specified message
+     * @param message The message
+     * @return The ApiError
+     */
+    public static ApiError notAcceptable(String message) {
+        return error(HttpStatus.NOT_ACCEPTABLE, message);
+    }
+
+    /**
+     * Creates a not acceptable error
+     * @return The ApiError
+     */
+    public static ApiError notAcceptable() {
+        return error(HttpStatus.NOT_ACCEPTABLE, "not found");
+    }
+
+    /**
+     * Creates a not found error with the specified exception or throwable
+     * @param t The exception or throwable
+     * @return The ApiError
+     */
     public static ApiError notFound(Throwable t) {
         return error(HttpStatus.NOT_FOUND, t);
     }
+
+    /**
+     * Creates a not found error with the specified message
+     * @param message The message
+     * @return The ApiError
+     */
     public static ApiError notFound(String message) {
         return error(HttpStatus.NOT_FOUND, message);
     }
+
+    /**
+     * Creates a not found error
+     * @return The ApiError
+     */
     public static ApiError notFound() {
         return error(HttpStatus.NOT_FOUND, "not found");
     }
+
+    /**
+     * Creates a not implemented error with the specified exception or throwable
+     * @param t The exception or throwable
+     * @return The ApiError
+     */
     public static ApiError notImplemented(Throwable t) {
         return error(HttpStatus.NOT_IMPLEMENTED, t);
     }
+
+    /**
+     * Creates a not implemented error with the specified message
+     * @param message The message
+     * @return The ApiError
+     */
     public static ApiError notImplemented(String message) {
         return error(HttpStatus.NOT_IMPLEMENTED, message);
     }
+
+    /**
+     * Creates a not implemented error
+     * @return The ApiError
+     */
     public static ApiError notImplemented() {
         return error(HttpStatus.NOT_IMPLEMENTED, "not implemented");
     }
+
+    /**
+     * Creates a request entity too large error with the specified exception or throwable
+     * @param t The exception or throwable
+     * @return The ApiError
+     */
+    public static ApiError requestEntityTooLarge(Throwable t) {
+        return error(HttpStatus.REQUEST_ENTITY_TOO_LARGE, t);
+    }
+
+    /**
+     * Creates a request entity too large error with the specified message
+     * @param message The message
+     * @return The ApiError
+     */
+    public static ApiError requestEntityTooLarge(String message) {
+        return error(HttpStatus.REQUEST_ENTITY_TOO_LARGE, message);
+    }
+
+    /**
+     * Creates a request entity too large error
+     * @return The ApiError
+     */
+    public static ApiError requestEntityTooLarge() {
+        return error(HttpStatus.NOT_IMPLEMENTED, "not implemented");
+    }
+
+    /**
+     * Creates an unauthorized error with the specified exception or throwable
+     * @param t The exception or throwable
+     * @return The ApiError
+     */
     public static ApiError unauthorized(Throwable t) {
         return error(HttpStatus.UNAUTHORIZED, t);
     }
+
+    /**
+     * Creates an unauthorized error with the specified  message
+     * @param message The message
+     * @return The ApiError
+     */
     public static ApiError unauthorized(String message) {
         return error(HttpStatus.UNAUTHORIZED, message);
     }
+
+    /**
+     * Creates an unauthorized error
+     * @return The ApiError
+     */
     public static ApiError unauthorized() {
         return error(HttpStatus.UNAUTHORIZED, "unauthorized");
     }
+
+    /**
+     * Creates an unsupported media type error with the specified exception or throwable
+     * @param t The exception or throwable
+     * @return The ApiError
+     */
     public static ApiError unsupportedMediaType(Throwable t) {
         return error(HttpStatus.UNSUPPORTED_MEDIA_TYPE, t);
     }
+
+    /**
+     * Creates an unsupported media type error with the specified message
+     * @param message The message
+     * @return The ApiError
+     */
     public static ApiError unsupportedMediaType(String message) {
         return error(HttpStatus.UNSUPPORTED_MEDIA_TYPE, message);
     }
+
+    /**
+     * Creates an unsupported media type error
+     * @return The ApiError
+     */
     public static ApiError unsupportedMediaType() {
         return error(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "unsupported media type");
     }
--- src/main/java/io/github/cdimascio/jwcperrors/HttpStatus.java
+++ src/main/java/io/github/cdimascio/jwcperrors/HttpStatus.java
@@ -5,11 +5,22 @@ public enum HttpStatus {
     BAD_REQUEST(400, "bad request"),
     CONFLICT(409, "conflict"),
     FORBIDDEN(403, "forbidden"),
+    GATEWAY_TIMEOUT(504, "gateway timeout"),
+    GONE(410, "gone"),
     INTERNAL_SERVER_ERROR(500, "internal server error"),
+    NOT_ACCEPTABLE(406, "not acceptable"),
     NOT_FOUND(404, "not found"),
     NOT_IMPLEMENTED(501, "not implemented"),
+    PRECONDITION_FAILED(412, "precondition failed"),
+    PRECONDITION_REQUIRED(428, "precondition required"),
+    PROXY_AUTHENTICATION_REQUIRED(407, "proxy authentication required"),
+    REQUEST_ENTITY_TOO_LARGE(413, "request entity too large"),
+    SERVICE_UNAVAILABLE(503, "service unavailable"),
     UNAUTHORIZED(401, "unauthorized"),
-    UNSUPPORTED_MEDIA_TYPE(415, "unsupported media type");
+    UNAVAILABLE_FOR_LEGAL_REASONS(451, "unavailable for legal reasons"),
+    UNSUPPORTED_MEDIA_TYPE(415, "unsupported media type"),
+    UPGRADE_REQUIRED(426, "upgrade required");
+
     private final int code;
     private final String description;
 
@@ -18,10 +29,18 @@ public enum HttpStatus {
         this.description = description;
     }
 
+    /**
+     * Returns http status code for this error
+     * @return An integer representing http status code
+     */
     public int getCode() {
         return this.code;
     }
 
+    /**
+     * The http status description
+     * @return
+     */
     public String getDescription() {
         return this.description;
     }
